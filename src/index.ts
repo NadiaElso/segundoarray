9d96b9123ac0aa89af5b897f75c5fccfffe4d0d6
NadiaElso <92063948+NadiaElso@users.noreply.github.com>
2021-11-01 21:48:23 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+//let dimension: number=Number(prompt("Ingrese la dimension"));
+let nombres: string[] = new Array(dimension);
+let index:number;
+
+for (let index = 0; index < nombres.length; index++) {
+  //nombres [index] = prompt("Ingrese un nombre que quiere poner en el lugar",${index},":");
+ 
+}
+
+for (let i = 0; i < nombres.length; i++) {
+  console.log("La persona que ingresó en la posicion",${i}"es":" ``${n});
+  
+}
